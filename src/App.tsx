d57ce03e19892e18716621065940b67579707c62
iRewiewer <irewiewer@gmail.com>
2025-05-14 13:01:43 +0300

    routing fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Routes, Route, Navigate, HashRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import SingleCardCreator from './pages/SingleCardCreator';
 import BatchCardCreator from './pages/BatchCardCreator';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 const App: React.FC = () => (
-    <HashRouter>
+    <Router basename={process.env.PUBLIC_URL}>
         <Navbar />
         <Routes>
             <Route path="/single" element={<SingleCardCreator />} />
             <Route path="/batch" element={<BatchCardCreator />} />
             <Route path="*" element={<Navigate to="/single" replace />} />
         </Routes>
-    </HashRouter>
+    </Router>
 );
 
 export default App;
