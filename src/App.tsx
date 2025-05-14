60c98725e373f0b5900e3590593e8b8483d7b5bb
iRewiewer <irewiewer@gmail.com>
2025-05-14 12:46:26 +0300

    quick fix for 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, HashRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import SingleCardCreator from './pages/SingleCardCreator';
 import BatchCardCreator from './pages/BatchCardCreator';
 
 const App: React.FC = () => (
-    <BrowserRouter basename="/SimpleCardCreator">
+    <HashRouter basename="/SimpleCardCreator">
         <Navbar />
         <Routes>
             <Route path="/single" element={<SingleCardCreator />} />
             <Route path="/batch" element={<BatchCardCreator />} />
             <Route path="*" element={<Navigate to="/single" replace />} />
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 export default App;
